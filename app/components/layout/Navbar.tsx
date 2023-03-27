435f17010eb6dc0c0b8c132ab574c52e6abee687
bogdan12893 <bogdan1281993@gmail.com>
2023-03-27 13:15:22 +0300

    user auth + protected route

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -1,17 +1,37 @@
 "use client";
 import Link from "next/link";
+import { signIn, signOut } from "next-auth/react";
 
-export default function Navbar() {
+export default function Navbar({ session }) {
   return (
-    <nav className=" bg-teal-800 p-4">
+    <nav className=" bg-teal-800 p-4 flex justify-between items-center">
       <ul className="flex justify-center">
-        <li className="mx-4">
+        <li className="mr-4">
           <Link href="/">Home</Link>
         </li>
-        <li>
+        <li className="mr-4">
           <Link href="/categories">Categories</Link>
         </li>
+        {session && (
+          <li className="mr-4">
+            <Link href="/profile">My profile</Link>
+          </li>
+        )}
       </ul>
+      <div>
+        {!session ? (
+          <button className="w-28 mr-3" onClick={() => signIn()}>
+            Sign in
+          </button>
+        ) : (
+          <div className="flex items-center">
+            <p>Hello, {session.user.name}!</p>
+            <button className="ml-3 w-28" onClick={() => signOut()}>
+              Sign Out
+            </button>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
+import { getServerSession } from "next-auth";
 import Navbar from "./components/layout/Navbar";
 import "./globals.css";
 import QueryWrapper from "./QueryWrapper";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { User } from "@prisma/client";
 
 export const metadata = {
   title: "Bikes App",
   description: "Bikes bikes bikes",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
       <body>
-        <Navbar />
+        <Navbar session={session} />
         <QueryWrapper>{children}</QueryWrapper>
       </body>
     </html>
