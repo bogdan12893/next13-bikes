56fb81318beb61643da26dc6fdc53a38bae529ad
bogdan12893 <bogdan1281993@gmail.com>
2023-03-29 16:16:05 +0300

    disable custom login page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   pages: {
-    signIn: "/login",
+    // signIn: "/login",
   },
   session: {
     strategy: "jwt",
