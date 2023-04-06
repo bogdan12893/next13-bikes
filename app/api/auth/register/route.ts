cba9306c91d03bea849c466b021204ae9d6f42d5
bogdan12893 <bogdan1281993@gmail.com>
2023-04-06 08:51:10 +0300

    implement email templates

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -30,10 +30,15 @@ export async function POST(request: Request) {
 
     const verificationLink = `${process.env.NEXT_PUBLIC_BASE_URL}/verify-email?token=${token}`;
 
+    const emailData = {
+      title: "Please verify your email!",
+      description: "Confirm email by clicking",
+    };
+
     await transporter.sendMail({
       ...mailOptions,
-      subject: `Please verify your email, ${name}`,
-      html: `<p>Please verify your email address by clicking <a href="${verificationLink}">this link</a>.</p>`,
+      subject: `Bike garage: Please verify your email, ${name}`,
+      html: `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Email</title><style type="text/css">body{background-color:#4b5563;font-family:"Courier New",Courier,monospace}.content{color:#fff;background-color:#1f2937;padding:20px;border-radius:20px;text-align:center}h1{font-size:30px}h2{font-size:20px}p{margin-bottom:30px}.link{background-color:#0c9488;color:#fff!important;padding:10px 20px;border-radius:10px;text-decoration:none;display:inline-block}</style></head><body><div class="content"><h1>◀︎ Bike Garage ▶︎</h1><h2>${emailData.title}</h2><p><span>${emailData.description}</span></p><div class="button-wrapper"><a href="${verificationLink}" class="link">Here</a></div></div></body></html>`,
     });
 
     return NextResponse.json({
