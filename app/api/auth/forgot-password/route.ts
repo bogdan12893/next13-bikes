fb2f4356f52164e779ef0b19e2acb5ad897a9073
bogdan12893 <bogdan1281993@gmail.com>
2023-04-07 10:44:22 +0300

    send emails to user

--- app/api/auth/forgot-password/route.ts
+++ app/api/auth/forgot-password/route.ts
@@ -27,6 +27,7 @@ export async function POST(request: Request) {
   try {
     await transporter.sendMail({
       ...mailOptions,
+      to: email,
       subject: `Bike garage: Reset your password`,
       html: `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Email</title><style type="text/css">body{background-color:#4b5563;font-family:"Courier New",Courier,monospace}.content{color:#fff;background-color:#1f2937;padding:20px;border-radius:20px;text-align:center}h1{font-size:30px}h2{font-size:20px}p{margin-bottom:30px}.link{background-color:#0c9488;color:#fff!important;padding:10px 20px;border-radius:10px;text-decoration:none;display:inline-block}</style></head><body><div class="content"><h1>◀︎ Bike Garage ▶︎</h1><h2>${emailData.title}</h2><p><span>${emailData.description}</span></p><div class="button-wrapper"><a href="${verificationLink}" class="link">Here</a></div></div></body></html>`,
     });
